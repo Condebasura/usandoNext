e17a9d11fd5f019a33d3eeffb3974dfc6f4f9e30
Pablo Zabala <could_2032@outlook.com>
2024-08-14 08:32:55 -0300

    Practica y aprendizaje

---
+++ src/app/_oculta/page.tsx
@@ -0,0 +1,4 @@
+export default function Hidden(){
+    return <h1>Ruta Oculta</h1>
+    // El gion bajo antes del nombre de la carpeta permite que la misma este oculta, para almacenar funciones auxiliares, compoentes , conexion a la db ,etc.
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,4 @@
+export default function NotFound(){
+return <h1>Error 404: Not Found</h1>;
+// Al crear not-found.tsx se crea la pagina 404 a la que se redirige cuando la url esta mal
+}
