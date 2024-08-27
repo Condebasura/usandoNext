e65f1d6079a35304d89e19a91bffd9aa479d1e4a
Pablo Zabala <could_2032@outlook.com>
2024-08-26 23:30:24 -0300

    Practica y aprendizaje(Metadata: default , absolute y template)

--- src/app/acerca/page.tsx
+++ src/app/acerca/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata ={
+    title: {
+        absolute: "Acerca", // Marca el title de la pagina en absoluto, ignora las demas metadatas
+    }, 
+    description: "Estas en la seccion de Acerca",
+};
+
 export default function Acerca(){
     return <h1>Acerca</h1>;
 }
--- src/app/productos/[productId]/page.tsx
+++ src/app/productos/[productId]/page.tsx
@@ -3,6 +3,7 @@ type Props = {params: {
     productId: string;
 };
 };
+// cambiar los titulos de los metadatos dinamicamente en el archivo de cada carpeta
 export const generateMetadata =({params,}: Props):Metadata =>{
     return{
         title: `Producto ${params.productId}`
