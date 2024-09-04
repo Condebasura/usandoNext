1cbf13dde949889385b1e68eeb55ea91f810d70a
Pablo Zabala <could_2032@outlook.com>
2024-09-04 00:38:03 -0300

    Practica y aprendizaje(template)

--- src/app/(Cuenta)/template.tsx
+++ src/app/(Cuenta)/template.tsx
@@ -1,12 +1,20 @@
+"use client";
+import { useState } from "react";
 export default function ProfileLayout(
    
     {children}:{
     children: React.ReactNode
     }) {
+        const [input , setInput] = useState("");
         return ( // Crear rutas relativas 
             <>
             <h2>Rutas rellativas a la cuenta del Usuario</h2>
             {children}
+            <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            />
             </>
         );
     }
