2b498825b904d6bf79901a92ced844ab4cdfdac6
Pablo Zabala <could_2032@outlook.com>
2024-09-10 21:35:33 -0300

    Practica y aprendizaje(api rest)

---
+++ src/app/api/products/data.ts
@@ -0,0 +1,14 @@
+export const products =[
+    {
+        id:1, 
+        product: "producto 1",
+    },
+    {
+        id:2, 
+        product: "producto 2",
+    },
+    {
+        id:3, 
+        product: "producto 3",
+    },
+]
